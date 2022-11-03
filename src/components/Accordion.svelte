0777be358a01c885217eb57bf3e15864f93b4bca
n1sym <owlbook248@gmail.com>
2022-11-03 23:26:53 +0900

    dist

---
+++ src/components/Accordion.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Item from './Item.svelte'
+
+  export let test;
+
+	const data = {
+		'続きを読む': [test],
+	}
+</script>
+{#each Object.entries(data) as entry}
+<Item {entry} />
+{/each}
---
+++ src/components/Item.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { slide } from "svelte/transition";
+	export let entry
+	let isOpen = false
+	const toggle = () => isOpen = !isOpen
+</script>
+<style>
+	button {border: none; background: none; display:block; color: inherit; font-size: 16px; cursor: pointer; margin: 0; padding: 0}
+</style>
+<button on:click={toggle} aria-expanded={isOpen}> {entry[0]} </button>
+{#if isOpen}
+<ul transition:slide={{ duration: 300 }}>
+	{#each entry[1] as item}
+	<li>{item}</li>
+	{/each}
+</ul>
+{/if}
